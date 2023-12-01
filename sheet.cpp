06008dfd3bac87aea40138634b6fe76478335048
HassanMuhamed1 <142698990+HassanMuhamed1@users.noreply.github.com>
2023-12-01 11:14:07 +0200

    Update sheet.cpp

--- sheet.cpp
+++ sheet.cpp
@@ -60,3 +60,58 @@ int main(){
     }*/
     return 0;
 }
+//===================================================================================================
+// Probem 3
+#include <iostream>
+#include <fstream>
+#include <map>
+#include <sstream>
+#include <cctype>
+using namespace std;
+string Clean(string& word) {
+    string cleanedWord;
+    for (char c : word) {
+        if (isalnum(c) || c == '-') {
+            cleanedWord += tolower(c);
+        }
+    }
+    return cleanedWord;
+}
+
+
+int main(){
+    string filename;
+
+    cout << "Enter the name of the text file: ";
+    cin >> filename;
+
+    ifstream inputFile(filename);
+
+    if (!inputFile.is_open()) {
+        cerr << "Error opening the file." <<endl;
+        return 1;
+    }
+
+    map<string, int> FrequencyTable;
+
+    string line;
+    while (getline(inputFile, line)) {
+        istringstream iss(line);
+        string word;
+
+        while (iss >> word) {
+            string cleanedWord = Clean(word);
+            if (!cleanedWord.empty()) {
+                FrequencyTable[cleanedWord]++;
+            }
+        }
+    }
+
+    inputFile.close();
+
+    cout << "Frequency Table:\n";
+    for (const auto& pair : FrequencyTable) {
+        cout << pair.first << ": " << pair.second << endl;
+    }
+    return 0;
+}
