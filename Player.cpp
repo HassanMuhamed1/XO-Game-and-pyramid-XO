7e34a38295825745a670d6438f0c0d54e338722f
Hassan Muhammed <142698990+HassanMuhamed1@users.noreply.github.com>
2023-12-14 23:22:11 +0200

    Update Player.cpp

--- Player.cpp
+++ Player.cpp
@@ -1,45 +1,39 @@
-// Class definition for XO_Player class
+// Class definition for XO_GameManager class
 // Author:  Mohammad El-Ramly
 // Date:    10/10/2022
 // Version: 1
-#include<iostream>
-#include<random>
-#include<iomanip>
-#include<algorithm>
-#include"BoardGame_Classes.hpp"
 
+#include <iostream>
+#include"BoardGame_Classes.hpp"
 using namespace std;
 
-// Give player a symbol to use in playing
-// It can be X or O or others
-// This is needed for computer players
-// as it does not ask for a name
-Player::Player(char symbol) {
-    this->symbol = symbol;
+GameManager::GameManager(Board* bPtr, Player* playerPtr[2] ) {
+    boardPtr = bPtr;
+    players[0] = playerPtr[0];
+    players[1] = playerPtr[1];
 }
 
-// Optionally, you can give him ID or order
-// Like Player 1 and Player 2
-Player::Player (int order, char symbol) {
-    cout << "Welcome player " << order << endl;
-    cout << "Please enter your name: ";
-    cin >> name;
-    this->symbol = symbol;
-}
+void GameManager::run() {
+    int x, y;
+    boardPtr->display_board();
 
-// Get desired move: x y (each between 0 and 2)
-// Virtual (can change for other player types)
-void Player::get_move (int& x, int& y) {
-    cout << "\nPlease enter your move x and y (0 to 2) separated by spaces: ";
-    cin >> x >> y;
-}
-
-// Give player info as a string
-string Player::to_string(){
-    return "Player: " + name ;
-}
-
-// Get symbol used by player
-char Player::get_symbol() {
-    return symbol;
+    while (!boardPtr->game_is_over()) {
+        for (int i:{0,1}) {
+            players[i]->get_move(x, y);
+            while (!boardPtr->update_board (x, y, players[i]->get_symbol())){
+                players[i]->get_move(x, y);
+            }
+            boardPtr->display_board();
+            if (boardPtr->is_winner()){
+                cout  << players[i]->to_string() << " wins\n";
+                return;
+            }
+            if (boardPtr->is_draw()){
+                cout << "Draw!\n";
+                return;
+            }
+        }
+    }
+    delete[]players[1];
+    delete[]players[0];
 }
