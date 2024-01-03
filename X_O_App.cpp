419d4d43d5d6762788c1f6233f791f8b662b68d8
Hassan Muhammed <142698990+HassanMuhamed1@users.noreply.github.com>
2024-01-03 16:40:09 +0200

    Update X_O_App.cpp

--- X_O_App.cpp
+++ X_O_App.cpp
@@ -19,14 +19,15 @@ int main() {
     cout << "Welcome to FCAI X-O Game. :)\n";
     cout<<"1-3x3 Tic Tac Toe \n";
     cout<<"2-Pyramid XO \n";
-    cout<<"Enter name or number of game which you want: ";
+    cout<<"Enter number of game which you want: ";
 
     int choiceGame;
     cin>>choiceGame;
 
-    cout << "Press 1 if you want to play with computer and 2 if multiplayer: ";
-    cin >> choice;
+
     if(choiceGame == 1 ){
+        cout << "Press 1 if you want to play with computer and 2 if multiplayer: ";
+        cin >> choice;
         if(choice == 1){
             players[1] = new RandomPlayer ('o', 3);
         }
@@ -35,9 +36,12 @@ int main() {
         }
         GameManager x_o_game (new X_O_Board(), players);
         x_o_game.run();
-
+        delete players[0];
+        delete players[1];
     }
     else if(choiceGame == 2){
+        cout << "Press 1 if you want to play with computer and 2 if multiplayer: ";
+        cin >> choice;
         if(choice == 1){
             players[1] = new RandomPlayer ('o', 3);
         }
@@ -46,11 +50,9 @@ int main() {
         }
         GameManager x_o_game (new X_O_Pyramid_Board(), players);
         x_o_game.run();
+        delete players[0];
+        delete players[1];
     }
-    
-
-   delete players[0];
-   delete players[1];
 
     system ("pause");
 }
